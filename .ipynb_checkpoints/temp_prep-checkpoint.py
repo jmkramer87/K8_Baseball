369d5d24df1ccbab01c761f740adb6b94e6a244c
Joey Kramer <jokramer2@hotmail.com>
2025-12-02 02:19:07 +0000

    updates

--- .ipynb_checkpoints/temp_prep-checkpoint.py
+++ .ipynb_checkpoints/temp_prep-checkpoint.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 
 #Data Preparation
-df = pd.read_csv('/data/baseball/K8_Baseball/data/Batters 22-24 Average Cleaned.csv', encoding='UTF-8')
+df = pd.read_csv('/data/Batters 22-24 Average Cleaned.csv', encoding='UTF-8')
 
 f = open("logs-jkd22.txt", "a")
 
-f.write("Starting Analysis\n")
+print("Starting Analysis\n")
 
 f.write(df.head())
 
 f.write(df.mean())
 
-f.write("Job complete!")
+print("Job complete!")
 
 f.close()
