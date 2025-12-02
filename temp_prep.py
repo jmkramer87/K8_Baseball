2ebab5194d408ffa1d0553f31049fc78b7ca3b4d
Joey Kramer <jokramer2@hotmail.com>
2025-12-02 02:38:33 +0000

    updates

--- temp_prep.py
+++ temp_prep.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 #Data Preparation
-df = pd.read_csv('/data/Batters 22-24 Average Cleaned.csv', encoding='UTF-8')
+df = pd.read_csv('Batters_22-24_Avg_Clean_Nameless.csv', encoding='UTF-8')
 
 f = open("logs-jkd22.txt", "a")
 
