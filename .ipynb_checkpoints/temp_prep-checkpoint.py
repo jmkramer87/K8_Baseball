e967e3a04251c5c1d15f113c535e2c02adacb945
Joey Kramer <jokramer2@hotmail.com>
2025-12-03 02:45:40 +0000

    updates

--- .ipynb_checkpoints/temp_prep-checkpoint.py
+++ .ipynb_checkpoints/temp_prep-checkpoint.py
@@ -4,7 +4,7 @@ import numpy as np
 #Data Preparation
 df = pd.read_csv('Batters_22-24_Avg_Clean_Nameless.csv', encoding='UTF-8')
 
-f = open("logs-jkd22.txt", "w+")
+f = open("logs-jkd22.txt", "w")
 
 print("Starting Analysis\n")
 
