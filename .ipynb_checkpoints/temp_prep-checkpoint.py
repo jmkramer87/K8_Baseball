c5bf532e9781d291e017e8b505b20128ca421c6d
Joey Kramer <jokramer2@hotmail.com>
2025-12-03 03:00:48 +0000

    test

--- .ipynb_checkpoints/temp_prep-checkpoint.py
+++ .ipynb_checkpoints/temp_prep-checkpoint.py
@@ -1,17 +1,17 @@
-import pandas as pd
-import numpy as np
+#import pandas as pd
+#import numpy as np
 
 #Data Preparation
-df = pd.read_csv('Batters_22-24_Avg_Clean_Nameless.csv', encoding='UTF-8')
+#df = pd.read_csv('Batters_22-24_Avg_Clean_Nameless.csv', encoding='UTF-8')
 
-f = open("logs-jkd22.txt", "w")
+#f = open("logs-jkd22.txt", "w")
 
 print("Starting Analysis\n")
 
-f.write(df.head())
+#f.write(df.head())
 
-f.write(df.mean())
+#f.write(df.mean())
 
 print("Job complete!")
 
-f.close()
+#f.close()
